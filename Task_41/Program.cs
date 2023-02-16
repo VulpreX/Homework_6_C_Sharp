ee84decc2541cf908e4c84ab6a63876fb77aeae0
Denis Prozorov <den.abraca4@gmail.com>
2023-02-16 21:00:43 +0300

    end

---
+++ Task_41/Program.cs
@@ -0,0 +1,27 @@
+﻿int ReadInt(string argument1)
+{
+    Console.Write($"Enter {argument1} - ");
+    int number;
+
+    while(!int.TryParse(Console.ReadLine(), out number))
+    {
+        System.Console.WriteLine(" You entered an invalid number, try again!");
+    }  
+
+    return number; 
+}
+void count(int number)
+{
+    int count= 0;
+    
+    while (number !=0 )
+    {                     
+       if(number > 0)
+        {
+            count++; 
+        }
+        number= ReadInt(" your number, to stop write 0");        
+    }
+    System.Console.WriteLine($" Sum numbers > 0 = {count}");
+}
+count(ReadInt(" your number, to stop write 0"));
---
+++ Task_43/Program.cs
@@ -0,0 +1,24 @@
+﻿int ReadInt(string argument1)
+{
+    Console.Write($"Enter {argument1}  ");
+    int number;
+
+    while(!int.TryParse(Console.ReadLine(), out number))
+    {
+        System.Console.WriteLine(" You entered an invalid number, try again!");
+    }
+    
+
+    return number; 
+}
+
+void SearchForAnIntersection(int k1,int b1, int k2, int b2)
+{
+    double x = Math.Round(new Random().NextDouble(), 2) * 10 ;
+    Console.WriteLine($"x={x}");
+    double[] array = new double [2];
+    array[0] = k1 * x + b1 ;
+    array[1] = k2 * x + b2 ;
+    Console.WriteLine($"Intersection point = ({array[0]};{array[1]})");
+}
+SearchForAnIntersection(ReadInt("k1="),ReadInt("b1="),ReadInt("k2="),ReadInt("b2="));
